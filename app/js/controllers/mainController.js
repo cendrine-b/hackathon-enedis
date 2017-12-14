44c3afa109fadd06da7d031b11a06233f05cbcaa
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-12-14 20:49:05 +0100

    Update node modules

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -4,7 +4,7 @@ angular.module('app').controller('MainController', function($scope) {
   angular.element(document).ready(function() {
     angular.element('#reader').html5_qrcode(function(data) { // start webcam
       console.log("QR Code result: ", data); // print QR code content
-      document.location.href="https://www.google.fr/webhp?hl=fr&sa=X&ved=0ahUKEwjuwI64zInYAhWBRBoKHcNSBmwQPAgD";
+      document.location.href=data;
       angular.element("#reader").html5_qrcode_stop(); // stop webcam
     }, function(error) {
       // console.log(error);
@@ -12,5 +12,24 @@ angular.module('app').controller('MainController', function($scope) {
       console.log(videoError);
     });
   });
+
+
+  $scope.countHeart = 5;
+
+
+  /* var updateCounter = function() {
+    $scope.countHeart++;
+  }
+  updateCounter(); */
+
+  /*
+    Si réponse vrai = ajoute cœur si ;
+    Si réponse fausse = countHeart -1;
+  */
+  /* if() {
+
+  } else {
+
+  } */
 
 });
